1e01abaccea54b7b69e4cd3238c8c88738d87bcb
JamesMacker <jamesmccormack652@gmail.com>
2024-04-08 13:17:38 +0100

    fifth commit

--- humidifierDjango/humidifierDjango/settings.py
+++ humidifierDjango/humidifierDjango/settings.py
@@ -13,7 +13,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+# Build paths inside the project like this: BASE
+# _DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 WEBSITE_DIR = os.path.join(BASE_DIR, 'website')
 
@@ -26,7 +27,7 @@ SECRET_KEY = "django-insecure-w7q#-le!j9*l*x8qvn+3@fptu((op0xowfp)x!4wtji+&%v(af
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["192.168.0.248"]
+ALLOWED_HOSTS = ["192.168.224.152"]
 
 
 # Application definition
